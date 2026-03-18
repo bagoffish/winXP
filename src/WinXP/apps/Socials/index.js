90a2b18d671bddd62aacf860fa3d113bc8ea3e33
coy <coyintern@gmail.com>
2026-03-18 09:23:50 +0900

    Update index.js

--- src/WinXP/apps/Socials/index.js
+++ src/WinXP/apps/Socials/index.js
@@ -191,7 +191,7 @@ function Socials({ onClose }) {
                 My Time Zone
               </a>
               <a
-                href="https://open.spotify.com/artist/567cGc3PvmxiZFQCwjcaRq
+                href="https://open.spotify.com/artist/567cGc3PvmxiZFQCwjcaRq"
                 target="_blank"
                 rel="noopener noreferrer"
               >
