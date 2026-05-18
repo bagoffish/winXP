14f12bce08b72562db6244734bb32a204b1a2a81
coy <c@bagof.fish>
2026-05-18 10:00:05 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
 import WinXP from 'WinXP';
+import WinterLetterVN from './components/WinterLetterVN';
 
 const App = () => {
+  const path = window.location.pathname;
+
+  if (path === '/vn') {
+    return <WinterLetterVN />;
+  }
+
   return <WinXP />;
 };
 
