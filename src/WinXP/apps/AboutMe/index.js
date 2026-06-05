c2ceecba2ac6e45ac55559e6e6e93a29aab57c8d
coy <c@bagof.fish>
2026-06-05 19:25:22 +0900

    Update index.js

--- src/WinXP/apps/AboutMe/index.js
+++ src/WinXP/apps/AboutMe/index.js
@@ -254,11 +254,10 @@ RECOMMENDATIONS —
 TV Shows: Mr. Robot & American Gods
 Visual Novels: Saya no Uta, Steins;Gate, Katawa Shoujo, Higurashi,
                Teaching Feeling, Your Wings My wings,  & SubaHibi
-Mangas: Tokyo Akazukin, Null-Meta, Sayonara Zetsubou Sensei, Ashizuri Suizokukan,
-        Shadow Star, Made in Abyss, Mai-chan’s Daily Life & Shimeji Simulation
-Animes: A Silent Voice, Monster, Inuyashiki, Bungo Stray Dogs,
-        Alien Nine, Evangelion, Parasyte, Nichijou, Gachiakuta,
-        Girls Last Tour, Haibane Renmei & `;
+Mangas: Tokyo Akazukin, Null-Meta, Sayonara Zetsubou Sensei, Ashizuri Suizokukan, Homunculus,
+        Shadow Star, Ichi the Killer, Made in Abyss, Mai-chan’s Daily Life & Shimeji Simulation
+Animes: A Silent Voice, Monster, Inuyashiki, Bungo Stray Dogs, Girls Last Tour, 
+        Alien Nine, Evangelion, Parasyte, Nichijou, Gachiakuta, Haibane Renmei & `;
 
 const ABOUT_TEXT_AFTER = ``;
 
