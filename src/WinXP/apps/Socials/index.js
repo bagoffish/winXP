c6bd7716e701f11d0f921949547d3fb904f17f4d
coy <c@bagof.fish>
2026-07-07 14:17:02 +0900

    Update index.js

--- src/WinXP/apps/Socials/index.js
+++ src/WinXP/apps/Socials/index.js
@@ -191,7 +191,7 @@ function Socials({ onClose }) {
                 SoundCloud
               </a>
               <a
-                href="https://open.spotify.com/artist/1zw5S00K1P3fYHzPqfRkFn"
+                href="https://open.spotify.com/artist/567cGc3PvmxiZFQCwjcaRq"
                 target="_blank"
                 rel="noopener noreferrer"
               >
